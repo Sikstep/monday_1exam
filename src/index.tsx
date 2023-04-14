3de7c66dc21bed21091574b90c736ed688ba9bff
Sikstep <Sikstep1988@gmail.com>
2023-04-14 18:25:27 +0300

    install react-redux 84% (а могло быть 100%=)) 1

--- src/index.tsx
+++ src/index.tsx
@@ -1,65 +1,18 @@
-import React from 'react'
-import { createStore } from 'redux'
-import { Provider, useSelector, useDispatch } from 'react-redux'
-import ReactDOM from 'react-dom'
+import {combineReducers, createStore} from 'redux'
 
-type StudentType = {
-    id: number
-    name: string
-    age: number
-}
-
-const initState = {
-    students:
-        [
-            {id: 1, name: 'Bob', age: 23},
-            {id: 2, name: 'Alex', age: 22}
-        ] as Array<StudentType>
-}
-type AddStudentAT = {
-    type: 'ADD-STUDENT'
-    name: string
-    age: number
-    id: number
-}
-
-type InitialStateType = typeof initState
-
-const studentsReducer = (state: InitialStateType = initState, action: AddStudentAT): InitialStateType => {
-    switch (action.type) {
-        case 'ADD-STUDENT':
-            return {
-                ...state,
-                students: [...state.students, {
-                    name: action.name,
-                    age: action.age,
-                    id: action.id
-                }]
-            }
-    }
+let initialState = {items: [{name: 'Dimych'}, {name: 'Ignat'}]}
+const usersReducer = (state = initialState, action: any) => {
     return state
 }
 
-const appStore = createStore(studentsReducer)
-type RootStateType = ReturnType<typeof studentsReducer>
+const store = createStore(combineReducers({
+    users: usersReducer
+}))
 
+store.subscribe(() => {
+    console.log('state changed')
+})
 
-const StudentList = () => {
-    const students = useSelector((state: RootStateType) => state.students)
-    return (
-        <ul>
-            {students.map(s => <li key={s.id}>{`${s.name}. ${s.age} years.`}</li>)}
-        </ul>
-    )
-}
-const App = () => {
-    return <StudentList/>
-}
+store.dispatch({type: 'ANY'})
 
-ReactDOM.render(<div>
-        <Provider store={appStore}>
-            <App/>
-        </Provider>
-    </div>,
-    document.getElementById('root')
-)
+// Что нужно написать вместо XXX, чтобы в консоли увидеть 'state changed'?
